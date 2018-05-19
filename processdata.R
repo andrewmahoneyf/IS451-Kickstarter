5120718f1b9d049dbe7124f26b4d1c48a911fd41
andrewmahoneyf <andrew.mahoney.f@gmail.com>
2018-05-19 15:11:23 -0700

    logistic regression and new charts

--- processdata.R
+++ processdata.R
@@ -21,7 +21,7 @@ kickstarter.df$successful <- ifelse(kickstarter.df$state == "successful", 1, 0)
 kickstarter.full.df <- kickstarter.df %>% mutate("total_days" = deadline - launched) %>%
   select(name, main_category, country, launched, deadline, total_days, usd_pledged_real, usd_goal_real, backers, state, successful) %>%
   rename(category = main_category, pledged = usd_pledged_real, goal = usd_goal_real) %>%
-  filter(total_days <= 500)
+  filter(total_days <= 500, goal <= 2000000)
 kickstarter.full.df$total_days <- as.numeric(kickstarter.full.df$total_days, units="days")
 
 # create data frame with only predictor variables
@@ -32,34 +32,6 @@ set.seed(1)
 train.index <- sample(1:nrow(kickstarter.df), nrow(kickstarter.df)*0.6)  
 train.df <- kickstarter.df[train.index, ]
 valid.df <- kickstarter.df[-train.index, ]
-
-
-################################# ggplots #####################################
-# plot of only successful kickstarters
-kickstarter.successful.df <- kickstarter.full.df %>% filter(successful == 1)
-
-country_plot <- ggplot(data = kickstarter.successful.df) +
-                  geom_point(aes(x = pledged, y = country, color = country)) +
-                  labs(title = "Country Funding")
-
-country_plot2 <- ggplot(data = kickstarter.successful.df, aes(country), position = position_stack(reverse = TRUE)) +
-                    geom_bar() +
-                    coord_flip() +
-                    labs(title = "Top Countries")
-
-category_plot <- ggplot(data = kickstarter.successful.df) +
-                    geom_point(aes(x = pledged, y = category, color = category)) +
-                    labs(title = "Categories Funding")
-
-category_plot2 <- ggplot(data = kickstarter.successful.df, aes(category), position = position_stack(reverse = TRUE)) +
-                    geom_bar() +
-                    coord_flip() +
-                    labs(title = "Top Categories")
-
-days_plot <- ggplot(data =  subset(kickstarter.successful.df, total_days <= 50 & total_days > 10), aes(total_days)) +
-                geom_bar() +
-                labs(title = "Average Days")
-
 
 ########################## classification trees ################################
 # default classification tree, optimized by pruning
@@ -74,6 +46,45 @@ default.ct.point.pred <- predict(default.ct, valid.df, type = "class")
 # generate confusion matrix for validation data
 confusionMatrix <- confusionMatrix(default.ct.point.pred, factor(valid.df$successful))
 fourfoldplot(confusionMatrix$table)
+
+################################# ggplots #####################################
+# remove outliers for graphs and regressions 
+kickstarter.df <- filter(kickstarter.df, goal >= 20000, goal <= 100000)
+
+# dataframe for successful kickstarters as well as percent of successful
+kickstarter.successful.df <- kickstarter.full.df %>% filter(successful == 1)
+
+kickstarter.category.percent.df <- kickstarter.df %>% group_by(successful, category) %>%
+  summarise (n = n()) %>% mutate(percentage = (n / sum(n)) * 100) %>%
+  filter(successful == 1)
+
+kickstarter.country.percent.df <- kickstarter.df %>% group_by(successful, country) %>%
+  summarise (n = n()) %>% mutate(percentage = (n / sum(n)) * 100) %>%
+  filter(successful == 1)
+
+# plots
+country_plot <- ggplot(data = kickstarter.successful.df) +
+                  geom_point(aes(x = pledged, y = country, color = country)) +
+                  labs(title = "Country Funding")
+
+country_plot2 <- ggplot(kickstarter.country.percent.df, aes(x = country, y = percentage)) + 
+                    geom_bar(stat = "identity") +
+                    coord_flip() +
+                    labs(title = "Top Countries by % Successful")
+
+days_plot <- ggplot(data =  subset(kickstarter.successful.df, total_days <= 50 & total_days > 10), aes(total_days)) +
+                geom_bar() +
+                labs(title = "Average Days")
+
+
+category_plot <- ggplot(data = kickstarter.successful.df) +
+                    geom_point(aes(x = pledged, y = category, color = category)) +
+                    labs(title = "Categories Funding")
+
+category_plot2 <- ggplot(kickstarter.category.percent.df, aes(x = category, y = percentage)) + 
+                    geom_bar(stat = "identity") +
+                    coord_flip() +
+                    labs(title = "Top Categories by % Successful")
 
 
 ####################### multiple linear regression ##############################
@@ -94,4 +105,28 @@ hist(all.residuals, breaks = 25, xlab = "Residuals")
 
 
 ############################ Logistic Regression #################################
+# create reference categories
+kickstarter.df$category <- factor(kickstarter.df$category)
+kickstarter.df$country <- factor(kickstarter.df$country)
 
+kickstarter.df$category <- relevel(kickstarter.df$category, ref = "Music")
+kickstarter.df$country <- relevel(kickstarter.df$country, ref = "US")
+
+# create training and validation sets
+set.seed(5)
+train.index <- sample(1:nrow(kickstarter.df), nrow(kickstarter.df)*0.6)  
+train.df <- kickstarter.df[train.index, ]
+valid.df <- kickstarter.df[-train.index, ]
+
+# run logistic model, and show coefficients 
+logit.reg <- glm(successful ~ ., data = train.df, family = "binomial")
+summary(logit.reg)
+
+# set the cutoff to be 0.5 and evaluate the model
+## use predict() with type = "response" to compute predicted probabilities
+logit.reg.pred <- predict(logit.reg, valid.df, type = "response")
+
+# Choose cutoff value and evaluate classification performance
+pred <- ifelse(logit.reg.pred > 0.5, 1, 0)
+
+confusionMatrix(factor(pred), factor(valid.df$successful))
